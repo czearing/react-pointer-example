b22a2bf15ad297a97f5f8a444bcb97b1e5bbe0e1
Caleb Zearing <czearing@outlook.com>
2021-07-27 09:58:14 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./styles.css";
+import { on } from "@fluentui/utilities";
+
+export default function App() {
+  const [cordinates, setCordinates] = React.useState({ left: 0, top: 0 });
+  const disposables = React.useRef<(() => void)[]>([]);
+  const elementSize = 80;
+
+  const onPointerMove = (ev) => {
+    setCordinates({
+      left: ev.pageX - elementSize / 2,
+      top: ev.pageY - elementSize / 2
+    });
+  };
+
+  const onPointerUp = (ev) => {
+    disposables.current.forEach((dispose) => dispose());
+    disposables.current = [];
+  };
+
+  const onPointerDown = (ev) => {
+    ev.target.setPointerCapture(ev.pointerId);
+
+    disposables.current.push(
+      on(window, "pointermove", onPointerMove, true),
+      on(window, "pointerup", onPointerUp, true)
+    );
+  };
+
+  const circleStyle = {
+    position: "absolute",
+    width: elementSize,
+    height: elementSize,
+    transform: `translate(${cordinates.left}px, ${cordinates.top}px)`,
+    backgroundColor: "red",
+    touchAction: "none"
+  };
+
+  return (
+    <div className="App">
+      <div className="test" onPointerDown={onPointerDown} style={circleStyle} />
+    </div>
+  );
+}
